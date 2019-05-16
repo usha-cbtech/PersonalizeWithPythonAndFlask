60ad2d57557cc3ca7386c8548aa328a2daa0b524
Usha <usha.kiran@colorbracket.com>
2019-05-16 18:46:44 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+import boto3
+import json
+import time
+import io
+
+personalize = boto3.client('personalize')
+print("personalize: ", personalize)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
